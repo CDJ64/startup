d40e1a15549d49164a07aeaf41c9350f39c5b98f
CDJ64 <jordan.colin@gmail.com>
2023-03-10 16:07:38 -1000

    New classes in button elements; new PieChart class

--- survey.js
+++ survey.js
@@ -1,2 +1,12 @@
+// Defining functions and objects
+
+class PieChart {
+    
+}
+
+
+
+// Code to execute when page is opened
+
 const playerNameEl = document.querySelector('.player-name');
 playerNameEl.textContent = localStorage.getItem('pizzaName') ?? 'Guest';
